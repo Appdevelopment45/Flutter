f4d37be4126e3bea308d5aad0b81bb142bc33024
Jyotsna-Akkinapalli <40540136+Jyotsna-Akkinapalli@users.noreply.github.com>
2023-12-12 15:37:17 +0530

    Create dashboard.dart

---
+++ dashboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Material App
+    return MaterialApp(
+
+      // Scaffold Widget
+        home: Scaffold(
+          appBar: AppBar(
+            // AppBar takes a Text Widget
+            // in it's title parameter
+            title: const Text('dashboard'),
+          ),
+          body: const Center(child: Text('Welcome To CMR Institute of Technology')),
+        ));
+  }
+}
